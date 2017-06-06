97e6c9ce4f331453babbbf193950a0da2216e491
lisa louison <lisa.louison@gmail.com>
2017-06-06 14:20:12 -0400

    updated index

--- client/src/components/Index.jsx
+++ client/src/components/Index.jsx
@@ -7,12 +7,9 @@ render() {
 return (
     <div className='main_container'>
         <div className='column'>
-            <p>Welcome to SkyCast Weather App</p>
+            <h1 className='logo'>SkyCast Weather</h1>
             <Link to='/login' className='button'>Login</Link>
             <Link to='/register' className='button'>Register</Link>
-            <footer>
-                <img className='attribution' src={require('../images/darksky.png')} />
-            </footer>
         </div>
     </div>
 );
