4f52f6c00e4b42000f66df6a3f84a2ae225d6b9a
lisa louison <lisa.louison@gmail.com>
2017-06-05 21:29:39 -0400

    creating config file

---
+++ db/config.js
@@ -0,0 +1,39 @@
+// setting up a variable that logs every query
+const options = {
+  connect: (client, dc, isFresh) => {
+    // do this every time the database connects
+    console.log('Connected to database:', client.connectionParameters.database)
+  },
+  query: (e) => {
+    // do this every time a query is made to the database
+    console.log('making query ========> ' + e.query);
+  },
+  receive: (data, result, e) => {
+    // do this every time the app receives data from the database
+    console.log('completed query =======> ' + e.query);
+  },
+  disconnect: (client, dc) => {
+    // do this every time the database disconnects (usually after every query)
+    console.log('Disconnecting from database:', client.connectionParameters.database);
+  }
+};
+
+// importing pg-promise
+const pgp = require('pg-promise')(options);
+
+// setting a variable for pg-promise
+let db;
+
+// creating if statements to determine the instance of pgp to use
+if (process.env.NODE_ENV === 'development' || !process.env.NODE_ENV) {
+    db = pgp({
+        database: 'weather_development',
+        port: 5432,
+        host: 'localhost'
+    });
+} else if (process.env.NODE_ENV === 'production') {
+    db = pgp(process.env.DATABASE_URL);
+}
+
+//exporting pg-promise
+module.exports = db;
