3a6a270ee378b17999726df0945321932e00afd6
lisa louison <lisa.louison@gmail.com>
2017-06-06 19:24:38 -0400

    styled weather components

--- client/src/components/CurrentWeather.jsx
+++ client/src/components/CurrentWeather.jsx
@@ -10,7 +10,6 @@ render() {
             <h3>{Math.ceil(this.props.weatherInfo.weatherInfo.currently.temperature)}&#8457;</h3>
             <div className={this.props.weatherInfo.weatherInfo.currently.icon} id='main_icon'></div>
             <h4>{this.props.weatherInfo.weatherInfo.currently.summary}</h4>
-            <hr/>
         </div>
     )
 }
